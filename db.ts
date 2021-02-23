a450ee282523000718888d008a8794872a0b0c0c
JP-1997 <jyotiprakash.jpl.99@gmail.com>
2021-02-23 23:54:17 +0530

    setup express, added GET /chats route

---
+++ db.ts
@@ -0,0 +1,71 @@
+export const messages = [
+  {
+    id: "1",
+    content: "Dattebayo !!",
+    createdAt: new Date(Date.now() - 60 * 1000 * 1000),
+  },
+  {
+    id: "2",
+    content: "Naruto-kun",
+    createdAt: new Date(Date.now() - 2 * 60 * 1000 * 1000),
+  },
+  {
+    id: "3",
+    content: "Boku wa Tobi.",
+    createdAt: new Date(Date.now() - 24 * 60 * 1000 * 1000),
+  },
+  {
+    id: "4",
+    content: "I'll Chidori you.",
+    createdAt: new Date(Date.now() - 14 * 24 * 60 * 1000 * 1000),
+  },
+  {
+    id: "5",
+    content: "I have one of the biggest flex in anime.",
+    createdAt: new Date(Date.now() - 11 * 28 * 60 * 1000 * 1000),
+  },
+  {
+    id: "6",
+    content: "Mind-tranfer jutsu",
+    createdAt: new Date(Date.now() - 12 * 45 * 60 * 1000 * 1000),
+  },
+];
+
+export const chats = [
+  {
+    id: "1",
+    name: "Naruto Uzumaki",
+    picture: "https://randomuser.me/api/portraits/thumb/men/4.jpg",
+    lastMessage: messages.find((m) => m.id === "1"),
+  },
+  {
+    id: "2",
+    name: "Hinata Hyuga",
+    picture: "https://randomuser.me/api/portraits/thumb/women/4.jpg",
+    lastMessage: messages.find((m) => m.id === "2"),
+  },
+  {
+    id: "3",
+    name: "Obito Uchiha",
+    picture: "https://randomuser.me/api/portraits/thumb/men/5.jpg",
+    lastMessage: messages.find((m) => m.id === "3"),
+  },
+  {
+    id: "4",
+    name: "Kakashi Hatake",
+    picture: "https://randomuser.me/api/portraits/thumb/men/6.jpg",
+    lastMessage: messages.find((m) => m.id === "4"),
+  },
+  {
+    id: "5",
+    name: "Rock Lee",
+    picture: "https://randomuser.me/api/portraits/thumb/men/7.jpg",
+    lastMessage: messages.find((m) => m.id === "5"),
+  },
+  {
+    id: "6",
+    name: "Ino Yamanaka",
+    picture: "https://randomuser.me/api/portraits/thumb/women/2.jpg",
+    lastMessage: messages.find((m) => m.id === "6"),
+  },
+];
